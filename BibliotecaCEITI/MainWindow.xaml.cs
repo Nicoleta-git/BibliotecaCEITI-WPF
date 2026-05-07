2f6c9526a695ec6c66d89258746bf386684eb694
Misha-cybernet <mihailmartenco10@gmail.com>
2026-05-07 10:32:44 +0300

    feat: add book image upload and dynamic metadata loading for authors...

--- BibliotecaCEITI/MainWindow.xaml.cs
+++ BibliotecaCEITI/MainWindow.xaml.cs
@@ -28,16 +28,11 @@ namespace BibliotecaCEITI
             Dashboard dashView = new Dashboard();
 
             MainContentContainer.Content = dashView;
-
-<<<<<<< HEAD
-
-=======
             // Initialize timer
             timer = new DispatcherTimer();
             timer.Interval = TimeSpan.FromSeconds(1);
             //timer.Tick += Timer_Tick;
             timer.Start();
->>>>>>> 182d611c19026e72ab6370e065047b7d46f11961
         }
 
         
