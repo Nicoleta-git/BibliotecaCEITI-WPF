1cf4c9884a5838ed2a2733e7823b16a036d96a74
valentin_0x <valentin_0x@BOOT-PC-001>
2026-05-08 18:29:14 +0300

    feat: connect main borrowing manager to ManualBorrow interface

--- BibliotecaCEITI/ManualBorrow.xaml.cs
+++ BibliotecaCEITI/ManualBorrow.xaml.cs
@@ -1,28 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace BibliotecaCEITI
 {
-    /// <summary>
-    /// Interaction logic for ManualBorrow.xaml
-    /// </summary>
     public partial class ManualBorrow : UserControl
     {
         public ManualBorrow()
         {
             InitializeComponent();
+        }
+
+        private void BtnInapoi_Click(object sender, RoutedEventArgs e) {
+            var mainWindow = Window.GetWindow(this) as MainWindow;
+            mainWindow?.ChangeView(new Borrow());
+        }
+        private void CmbSelecteaza_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+        
+        }
+        private void CmbGrupa_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+        
+        }
+        private void ChkBifeazaToti_Changed(object sender, RoutedEventArgs e) { 
+        
+        }
+        private void BtnSalveaza_Click(object sender, RoutedEventArgs e) {
+        
         }
     }
 }
