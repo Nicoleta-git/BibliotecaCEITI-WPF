3dda3f5a61d55fbb9166eadb01a73d565f63cd4a
Misha-cybernet <mihailmartenco10@gmail.com>
2026-05-14 09:55:18 +0300

    feat: finalize automated loan infrastructure and batch distribution ...

--- BibliotecaCEITI/BookDetails.xaml.cs
+++ BibliotecaCEITI/BookDetails.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.Win32;
 using MySql.Data.MySqlClient;
+using Mysqlx.Crud;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,14 +24,82 @@ namespace BibliotecaCEITI
     /// </summary>
     public partial class BookDetails : UserControl
     {
-        string connectionString = "Server=127.0.0.1;Port=3306;Database=biblioteca_ceiti_go;Uid=root;Pwd=Mihail2026;";
-        public BookDetails()
+        private int _idCarte;
+        private string functie = "salvare";
+        public BookDetails(int idCarteSelectata = 0)
         {
             InitializeComponent();
             PopuleazaAutori();
             PopuleazaCategorii();
             PopuleazaEdituri();
             PopuleazaLimbi();
+
+            _idCarte = idCarteSelectata;
+
+            if (_idCarte > 0)
+            {
+                functie = "editare";
+                IncarcaDateEditare();
+            } else
+            {
+                functie = "salvare";
+            }
+        }
+
+        private void IncarcaDateEditare()
+        {
+            try
+            {
+                using (MySqlConnection conn = DatabaseConfig.GetConnection())
+                {
+                    conn.Open();
+                    string query = "sp_date_carte_selectata";
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@p_id", _idCarte);
+
+                        using (MySqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                txtTitlu.Text = reader["Titlu"].ToString();
+                                txtIsbn.Text = reader["ISBN"].ToString();
+                                txtDescriere.Text = reader["Descriere"] != DBNull.Value ? reader["Descriere"].ToString() : "";
+                                txtAnPublicare.Text = reader["An_Publicare"].ToString();
+                                txtPretMdl.Text = reader["Pret_Vanzare"].ToString();
+                                txtPretChirie.Text = reader["Pret_Chirie"].ToString();
+                                cbAutor.SelectedValue = reader["Autor"] != DBNull.Value ? Convert.ToInt32(reader["Autor"]) : -1;
+                                cbCategorie.SelectedValue = reader["Categorie"] != DBNull.Value ? Convert.ToInt32(reader["Categorie"]) : -1;
+                                cbEditura.SelectedValue = reader["Editura"] != DBNull.Value ? Convert.ToInt32(reader["Editura"]) : -1;
+                                cbLimba.SelectedValue = reader["Limba"] != DBNull.Value ? Convert.ToInt32(reader["Limba"]) : -1;
+                            }
+                            if (reader["Imagine"] != DBNull.Value)
+                            {
+                                byte[] copertaBytes = (byte[])reader["Imagine"];
+                                using (MemoryStream ms = new MemoryStream(copertaBytes))
+                                {
+                                    BitmapImage bitmap = new BitmapImage();
+                                    bitmap.BeginInit();
+                                    bitmap.StreamSource = ms;
+                                    bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                                    bitmap.EndInit();
+                                    bitmap.Freeze();
+                                    imgCoperta.Source = bitmap;
+                                }
+                            }
+                            else
+                            {
+                                imgCoperta.Source = null;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Eroare la încărcarea detaliilor cărții: " + ex.Message);
+            }
         }
 
         private void BtnBack_Click(object sender, RoutedEventArgs e)
@@ -72,21 +142,33 @@ namespace BibliotecaCEITI
 
             try
             {
-                using (MySqlConnection conn = new MySqlConnection(connectionString))
-                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                MySqlConnection conn = DatabaseConfig.GetConnection();
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                conn.Open();
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                List<ComboItem> lista = new List<ComboItem>();
+
+                foreach (DataRow rand in dt.Rows)
                 {
-                    conn.Open();
-                    MySqlDataAdapter da = new MySqlDataAdapter(cmd);
-                    DataTable dt = new DataTable();
-                    da.Fill(dt);
-                    cbAutor.ItemsSource = dt.DefaultView;
-                    cbAutor.DisplayMemberPath = "nume";
-                    cbAutor.SelectedValuePath = "id";
+                    ComboItem item = new ComboItem();
+                    item.Id = Convert.ToInt32(rand["id"]);
+                    item.Denumire = rand["nume"].ToString();
+                    lista.Add(item);
                 }
+
+                cbAutor.ItemsSource = lista;
+                cbAutor.DisplayMemberPath = "Denumire";
+                cbAutor.SelectedValuePath = "Id";
+
+                conn.Close();
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Eroare la popularea listei: " + ex.Message);
+                MessageBox.Show("Eroare: " + ex.Message);
             }
         }
 
@@ -96,21 +178,33 @@ namespace BibliotecaCEITI
 
             try
             {
-                using (MySqlConnection conn = new MySqlConnection(connectionString))
-                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                MySqlConnection conn = DatabaseConfig.GetConnection();
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                conn.Open();
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                List<ComboItem> lista = new List<ComboItem>();
+
+                foreach (DataRow rand in dt.Rows)
                 {
-                    conn.Open();
-                    MySqlDataAdapter da = new MySqlDataAdapter(cmd);
-                    DataTable dt = new DataTable();
-                    da.Fill(dt);
-                    cbCategorie.ItemsSource = dt.DefaultView;
-                    cbCategorie.DisplayMemberPath = "denumire";
-                    cbCategorie.SelectedValuePath = "id";
+                    ComboItem item = new ComboItem();
+                    item.Id = Convert.ToInt32(rand["id"]);
+                    item.Denumire = rand["denumire"].ToString();
+                    lista.Add(item);
                 }
+
+                cbCategorie.ItemsSource = lista;
+                cbCategorie.DisplayMemberPath = "Denumire";
+                cbCategorie.SelectedValuePath = "Id";
+
+                conn.Close();
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Eroare la popularea listei: " + ex.Message);
+                MessageBox.Show("Eroare: " + ex.Message);
             }
         }
 
@@ -120,21 +214,33 @@ namespace BibliotecaCEITI
 
             try
             {
-                using (MySqlConnection conn = new MySqlConnection(connectionString))
-                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                MySqlConnection conn = DatabaseConfig.GetConnection();
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                conn.Open();
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                List<ComboItem> lista = new List<ComboItem>();
+
+                foreach (DataRow rand in dt.Rows)
                 {
-                    conn.Open();
-                    MySqlDataAdapter da = new MySqlDataAdapter(cmd);
-                    DataTable dt = new DataTable();
-                    da.Fill(dt);
-                    cbEditura.ItemsSource = dt.DefaultView;
-                    cbEditura.DisplayMemberPath = "denumire";
-                    cbEditura.SelectedValuePath = "id";
+                    ComboItem item = new ComboItem();
+                    item.Id = Convert.ToInt32(rand["id"]);
+                    item.Denumire = rand["denumire"].ToString();
+                    lista.Add(item);
                 }
+
+                cbEditura.ItemsSource = lista;
+                cbEditura.DisplayMemberPath = "Denumire";
+                cbEditura.SelectedValuePath = "Id";
+
+                conn.Close();
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Eroare la popularea listei: " + ex.Message);
+                MessageBox.Show("Eroare: " + ex.Message);
             }
         }
 
@@ -144,21 +250,33 @@ namespace BibliotecaCEITI
 
             try
             {
-                using (MySqlConnection conn = new MySqlConnection(connectionString))
-                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                MySqlConnection conn = DatabaseConfig.GetConnection();
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                conn.Open();
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                List<ComboItem> lista = new List<ComboItem>();
+
+                foreach (DataRow rand in dt.Rows)
                 {
-                    conn.Open();
-                    MySqlDataAdapter da = new MySqlDataAdapter(cmd);
-                    DataTable dt = new DataTable();
-                    da.Fill(dt);
-                    cbLimba.ItemsSource = dt.DefaultView;
-                    cbLimba.DisplayMemberPath = "denumire";
-                    cbLimba.SelectedValuePath = "id";
+                    ComboItem item = new ComboItem();
+                    item.Id = Convert.ToInt32(rand["id"]);
+                    item.Denumire = rand["denumire"].ToString();
+                    lista.Add(item);
                 }
+
+                cbLimba.ItemsSource = lista;
+                cbLimba.DisplayMemberPath = "Denumire";
+                cbLimba.SelectedValuePath = "Id";
+
+                conn.Close();
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Eroare la popularea listei: " + ex.Message);
+                MessageBox.Show("Eroare: " + ex.Message);
             }
         }
 
@@ -171,22 +289,139 @@ namespace BibliotecaCEITI
             }
         }
 
-        private void btnSalveazaModificari_Click(object sender, RoutedEventArgs e)
+        private async Task AddBookAsync(string titlu, string autor, string categorie, string descriere, string isbn, string editura, int anPublicare, string limba, double pretVanzare, double pretChirie, byte[]? copertaBytes, int idBibliotecar)
         {
-            AddBook();
+            using (MySqlConnection conn = DatabaseConfig.GetConnection())
+            using (var cmd = new MySqlCommand("sp_insert_carte", conn))
+            {
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@p_titlu", titlu);
+                cmd.Parameters.AddWithValue("@p_autor", autor);
+                cmd.Parameters.AddWithValue("@p_categorie", categorie);
+                cmd.Parameters.AddWithValue("@p_descriere", (object?)descriere ?? DBNull.Value);
+                cmd.Parameters.AddWithValue("@p_isbn", (object?)isbn ?? DBNull.Value);
+                cmd.Parameters.AddWithValue("@p_editura", (object?)editura ?? DBNull.Value);
+                cmd.Parameters.AddWithValue("@p_an_publicare", anPublicare);
+                cmd.Parameters.AddWithValue("@p_limba", limba);
+                cmd.Parameters.AddWithValue("@p_pret_mdl", pretVanzare);
+                cmd.Parameters.AddWithValue("@p_pret_chirie_mdl", pretChirie);
+
+                var pBlob = cmd.Parameters.Add("@p_coperta", MySqlDbType.LongBlob);
+                pBlob.Value = (object?)copertaBytes ?? DBNull.Value;
+                cmd.Parameters.AddWithValue("@p_creat_de", idBibliotecar);
+
+                var pIdCarteNou = cmd.Parameters.Add("@p_id_carte_nou", MySqlDbType.UInt32);
+                pIdCarteNou.Direction = System.Data.ParameterDirection.Output;
+
+                var pCod = cmd.Parameters.Add("@p_cod", MySqlDbType.Int32);
+                pCod.Direction = System.Data.ParameterDirection.Output;
+                var pMsg = cmd.Parameters.Add("@p_mesaj", MySqlDbType.VarChar, 255);
+                pMsg.Direction = System.Data.ParameterDirection.Output;
+
+                await conn.OpenAsync();
+                await cmd.ExecuteNonQueryAsync();
+            }
         }
 
-        private void AddBook()
+        private async Task UpdateBookAsync(int idCarte, string titlu, string autor, string categorie, string descriere, string isbn, string editura, int anPublicare, string limba, double pretVanzare, double pretChirie, byte[]? copertaBytes, int idBibliotecar)
         {
-            using (var conn = new MySqlConnection(this.connectionString))
+            using (MySqlConnection conn = DatabaseConfig.GetConnection())
+            using (var cmd = new MySqlCommand("sp_update_carte", conn))
             {
-                string query = "INSERT INTO carti (titlu, isbn) VALUES (@titlu, @isbn)";
-                MySqlCommand cmd = new MySqlCommand(query, conn);
-                //cmd.Parameters.AddWithValue("@titlu", titlu);
-                //cmd.Parameters.AddWithValue("@isbn", isbn);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
 
-                conn.Open();
-                cmd.ExecuteNonQuery();
+                cmd.Parameters.AddWithValue("@p_id", idCarte);
+                cmd.Parameters.AddWithValue("@p_titlu", titlu);
+                cmd.Parameters.AddWithValue("@p_autor", autor);
+                cmd.Parameters.AddWithValue("@p_categorie", categorie);
+                cmd.Parameters.AddWithValue("@p_descriere", (object?)descriere ?? DBNull.Value);
+                cmd.Parameters.AddWithValue("@p_isbn", (object?)isbn ?? DBNull.Value);
+                cmd.Parameters.AddWithValue("@p_editura", (object?)editura ?? DBNull.Value);
+                cmd.Parameters.AddWithValue("@p_an_publicare", anPublicare);
+                cmd.Parameters.AddWithValue("@p_limba", limba);
+                cmd.Parameters.AddWithValue("@p_pret_mdl", pretVanzare);
+                cmd.Parameters.AddWithValue("@p_pret_chirie_mdl", pretChirie);
+
+                var pBlob = cmd.Parameters.Add("@p_coperta", MySqlDbType.LongBlob);
+                pBlob.Value = (object?)copertaBytes ?? DBNull.Value;
+                cmd.Parameters.AddWithValue("@p_modificat_de", idBibliotecar);
+
+                var pCod = cmd.Parameters.Add("@p_cod", MySqlDbType.Int32);
+                pCod.Direction = System.Data.ParameterDirection.Output;
+                var pMsg = cmd.Parameters.Add("@p_mesaj", MySqlDbType.VarChar, 255);
+                pMsg.Direction = System.Data.ParameterDirection.Output;
+
+                await conn.OpenAsync();
+                await cmd.ExecuteNonQueryAsync();
+            }
+        }
+
+        private async void btnSalveazaModificari_Click(object sender, RoutedEventArgs e)
+        {
+            btnSalveazaModificari.IsEnabled = false;
+
+            try
+            {
+                int idBibliotecarLogat = 1;
+                byte[]? copertaBytes = ImageToBytes(imgCoperta);
+
+                if (!int.TryParse(txtAnPublicare.Text, out int an))
+                {
+                    MessageBox.Show("Anul publicării este invalid.");
+                    return;
+                }
+
+                if (!double.TryParse(txtPretMdl.Text, out double pret))
+                {
+                    MessageBox.Show("Prețul este invalid.");
+                    return;
+                }
+
+                if (!double.TryParse(txtPretChirie.Text, out double chirie))
+                {
+                    MessageBox.Show("Prețul chiriei este invalid.");
+                    return;
+                }
+
+                if (functie == "salvare")
+                {
+                    await AddBookAsync(txtTitlu.Text, cbAutor.Text, cbCategorie.Text, txtDescriere.Text, txtIsbn.Text, cbEditura.Text, an, cbLimba.Text, pret, chirie, copertaBytes, idBibliotecarLogat);
+                }
+                else if (functie == "editare")
+                {
+                    await UpdateBookAsync(_idCarte, txtTitlu.Text, cbAutor.Text, cbCategorie.Text, txtDescriere.Text, txtIsbn.Text, cbEditura.Text, an, cbLimba.Text, pret, chirie, copertaBytes, idBibliotecarLogat);
+                }
+                var mainWindow = Window.GetWindow(this) as MainWindow;
+                if (mainWindow != null)
+                {
+                    mainWindow.MainContentContainer.Content = new Books();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("A apărut o eroare la salvare: " + ex.Message, "Eroare", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            finally
+            {
+                btnSalveazaModificari.IsEnabled = true;
+            }
+        }
+        private byte[]? ImageToBytes(System.Windows.Controls.Image imgControl)
+        {
+            if (imgControl.Source == null)
+                return null;
+
+            var bitmapSource = imgControl.Source as System.Windows.Media.Imaging.BitmapSource;
+            if (bitmapSource == null)
+                return null;
+
+            using (var ms = new System.IO.MemoryStream())
+            {
+                var encoder = new System.Windows.Media.Imaging.PngBitmapEncoder();
+                encoder.Frames.Add(System.Windows.Media.Imaging.BitmapFrame.Create(bitmapSource));
+                encoder.Save(ms);
+                return ms.ToArray();
             }
         }
 
--- BibliotecaCEITI/Dashboard.xaml.cs
+++ BibliotecaCEITI/Dashboard.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,7 +27,168 @@ namespace BibliotecaCEITI
         public Dashboard()
         {
             InitializeComponent();
- 
+            AfiseazaNumarTotal_Carti();
+            AfiseazaNumarTotal_Disponibile();
+            AfiseazaNumarTotal_Imprumuturi();
+            AfiseazaNumarTotal_Rezervari();
+            Top3Carti();
         }
+
+        private void AfiseazaNumarTotal_Carti()
+        {
+            string query = "CALL sp_total_carti();";
+
+            try
+            {
+                MySqlConnection conn = DatabaseConfig.GetConnection();
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                conn.Open();
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                total_carti.Text = dt.Rows[0]["TotalCarti"].ToString();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Eroare: " + ex.Message);
+            }
+        }
+
+        private void AfiseazaNumarTotal_Disponibile()
+        {
+            string query = "CALL sp_total_disponibile();";
+
+            try
+            {
+                MySqlConnection conn = DatabaseConfig.GetConnection();
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                conn.Open();
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                numar_disponibile.Text = dt.Rows[0]["TotalDisponibile"].ToString();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Eroare: " + ex.Message);
+            }
+        }
+
+        private void AfiseazaNumarTotal_Imprumuturi()
+        {
+            string query = "CALL sp_total_imprumuturi();";
+
+            try
+            {
+                MySqlConnection conn = DatabaseConfig.GetConnection();
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                conn.Open();
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                imprumuturi.Text = dt.Rows[0]["TotalImprumuturi"].ToString();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Eroare: " + ex.Message);
+            }
+        }
+        private void AfiseazaNumarTotal_Rezervari()
+        {
+            string query = "CALL sp_total_rezervari();";
+
+            try
+            {
+                MySqlConnection conn = DatabaseConfig.GetConnection();
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                conn.Open();
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                rezervari.Text = dt.Rows[0]["TotalRezervari"].ToString();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Eroare: " + ex.Message);
+            }
+        }
+
+        private void Top3Carti()
+        {
+            using (MySqlConnection conn = DatabaseConfig.GetConnection())
+            {
+                conn.Open();
+
+                using (MySqlCommand cmd = new MySqlCommand("sp_returneaza_top3_carti_imprumutate", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        int index = 0;
+                        while (reader.Read() && index < 3)
+                        {
+                            string titlu = reader["Carte"].ToString();
+                            string autor = reader["Autor"].ToString();
+                            int imprumuturi = Convert.ToInt32(reader["Imprumuturi"]);
+                            byte[] copertaBytes = reader["Coperta"] == DBNull.Value? null : (byte[])reader["Coperta"];
+                            ImageBrush brush = ConvertToImageBrush(copertaBytes);
+
+                            if (index == 0)
+                            {
+                                titlu_1.Text = titlu;
+                                autor_1.Text = autor;
+                                imprumuturi_1.Text = imprumuturi.ToString();
+                                coperta_1.Background = brush;
+                            }
+                            else if (index == 1)
+                            {
+                                titlu_2.Text = titlu;
+                                autor_2.Text = autor;
+                                imprumuturi_2.Text = imprumuturi.ToString();
+                                coperta_2.Background = brush;
+                            }
+                            else if (index == 2)
+                            {
+                                titlu_3.Text = titlu;
+                                autor_3.Text = autor;
+                                imprumuturi_3.Text = imprumuturi.ToString();
+                                coperta_3.Background = brush;
+                            }
+
+                            index++;
+                        }
+                    }
+                }
+            }
+        }
+
+        private ImageBrush ConvertToImageBrush(byte[] imageBytes)
+        {
+            if (imageBytes != null)
+            {
+                using (MemoryStream ms = new MemoryStream(imageBytes))
+                {
+                    BitmapImage bitmap = new BitmapImage();
+                    bitmap.BeginInit();
+                    bitmap.StreamSource = ms;
+                    bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                    bitmap.EndInit();
+                    bitmap.Freeze();
+                    return new ImageBrush(bitmap);
+                }
+            } else
+            {
+                return null;
+            }
+        }
+
     }
 }
