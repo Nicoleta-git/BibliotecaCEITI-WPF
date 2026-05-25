30006969b09d33b64be968247113733c5335bdb8
Misha-cybernet <mihailmartenco10@gmail.com>
2026-05-25 19:23:25 +0300

    fix: intercept background rendering execution crashes within Unhandl...

--- BibliotecaCEITI/App.xaml.cs
+++ BibliotecaCEITI/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace BibliotecaCEITI
 {
@@ -9,6 +10,32 @@ namespace BibliotecaCEITI
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            DispatcherUnhandledException += (s, ex) =>
+            {
+                if (ex.Exception is InvalidOperationException &&
+                    ex.Exception.Message.Contains("not an ancestor"))
+                {
+                    ex.Handled = true;
+                    return;
+                }
+            };
+
+            EventManager.RegisterClassHandler(
+                typeof(DataGrid),
+                DataGrid.LoadedEvent,
+                new RoutedEventHandler((s, ev) =>
+                {
+                    if (s is DataGrid dg)
+                    {
+                        dg.SetValue(VirtualizingPanel.IsVirtualizingProperty, false);
+                        dg.SetValue(ScrollViewer.CanContentScrollProperty, false);
+                    }
+                }));
+        }
     }
 
 }
