5a6af0376cf833960b7e0fd3c62ad4722bc388fa
Misha-cybernet <mihailmartenco10@gmail.com>
2026-05-25 21:25:25 +0300

    fix nested scrollviews on the dashboard

--- BibliotecaCEITI/App.xaml.cs
+++ BibliotecaCEITI/App.xaml.cs
@@ -10,32 +10,6 @@ namespace BibliotecaCEITI
     /// </summary>
     public partial class App : Application
     {
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            base.OnStartup(e);
-
-            DispatcherUnhandledException += (s, ex) =>
-            {
-                if (ex.Exception is InvalidOperationException &&
-                    ex.Exception.Message.Contains("not an ancestor"))
-                {
-                    ex.Handled = true;
-                    return;
-                }
-            };
-
-            EventManager.RegisterClassHandler(
-                typeof(DataGrid),
-                DataGrid.LoadedEvent,
-                new RoutedEventHandler((s, ev) =>
-                {
-                    if (s is DataGrid dg)
-                    {
-                        dg.SetValue(VirtualizingPanel.IsVirtualizingProperty, false);
-                        dg.SetValue(ScrollViewer.CanContentScrollProperty, false);
-                    }
-                }));
-        }
     }
 
 }
